249ee1e24e95b1744bddcd8dbf2a794bdfba1f93
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-18 21:36:58 +0000

    Enable deployment to Brew taps via the deploy_brew rule (#79)
    ## What is the goal of this PR?
    To enable deployment to Brew taps (closes https://github.com/graknlabs/grakn/issues/5032)
    
    ## What are the changes implemented in this PR?
    
    1. Implement `deploy_brew` which allows us to deploy to Brew taps
    2. Implement a checksum calculation rule `checksum` which is needed by `deploy_brew` to verify checksum
    
    Usage example:
    ```
    # BUILD
    deploy_brew(
        name = "deploy-brew",
        checksum = "//:checksum",
        deployment_properties = "@graknlabs_build_tools//:deployment.properties",
        formula = "//config/brew:grakn-core.rb",
        version_file = "//:VERSION"
    )
    
    checksum(
        name = "checksum",
        target = ":assemble-mac-zip"
    )
    
    # deployment.properties
    repo.brew.test=https://github.com/graknlabs/homebrew-tap-test/
    repo.brew.release=https://github.com/graknlabs/homebrew-tap/
    
    # Running the command
    GRABL_CREDENTIAL=... bazel run //:deploy-brew -- test
    ```

--- brew/rules.bzl
+++ brew/rules.bzl
@@ -1,28 +1,56 @@
 def _deploy_brew_impl(ctx):
-    ctx.actions.run_shell(
-        inputs = [ctx.file._deployment_script_template, ctx.file.version_file],
-        outputs = [ctx.outputs.deployment_script],
-        command = "VERSION=`cat %s` && sed -e s/{brew_version}/$VERSION/g %s > %s" % (
-            ctx.file.version_file.path, ctx.file._deployment_script_template.path, ctx.outputs.deployment_script.path)
+    ctx.actions.expand_template(
+        template = ctx.file._deploy_brew_template,
+        output = ctx.outputs.deployment_script,
+        substitutions = {},
+        is_executable = True
+    )
+    return DefaultInfo(
+        runfiles = ctx.runfiles(
+            files = [
+                ctx.file.checksum,
+                ctx.file.deployment_properties,
+                ctx.file.formula,
+                ctx.file.version_file
+            ],
+            symlinks = {
+                'checksum.sha256': ctx.file.checksum,
+                'deployment.properties': ctx.file.deployment_properties,
+                'formula': ctx.file.formula,
+                'VERSION': ctx.file.version_file
+            }
+        ),
+        executable = ctx.outputs.deployment_script
     )
 
-    return DefaultInfo(executable = ctx.outputs.deployment_script)
 
 deploy_brew = rule(
     attrs = {
-        "version_file": attr.label(
+        "_deploy_brew_template": attr.label(
+            allow_single_file = True,
+            default = "//brew/templates:deploy.py"
+        ),
+        "checksum": attr.label(
             allow_single_file = True,
             mandatory = True,
-            doc = "File containing version string"
         ),
-        "_deployment_script_template": attr.label(
+        "deployment_properties": attr.label(
             allow_single_file = True,
-            default = "//brew/templates:deploy.py",
+            mandatory = True
         ),
+        "formula": attr.label(
+            allow_single_file = True,
+            mandatory = True,
+            doc = "The brew formula definition"
+        ),
+        "version_file": attr.label(
+            allow_single_file = True,
+            mandatory = True
+        )
     },
     executable = True,
     outputs = {
-          "deployment_script": "%{name}.sh",
+        "deployment_script": "%{name}.py"
     },
     implementation = _deploy_brew_impl
 )
--- brew/templates/deploy.py
+++ brew/templates/deploy.py
@@ -2,31 +2,85 @@
 
 from __future__ import print_function
 import os
-import subprocess
+import shutil
+import subprocess as sp
 import sys
-
-VERSION = '{brew_version}'
-URL = 'https://github.com/graknlabs/grakn/releases/download/{brew_version}/grakn-core-{brew_version}.zip'
-URL_ARG = '--url={}'.format(URL)
+import tempfile
+import urllib
 
 
-def check_call(cmd, exec_msg=None, error_msg='Error: command execution failed'):
-    try:
-        if not exec_msg:
-            exec_msg = 'Executing command: {}'.format(cmd)
-        print(exec_msg)
-        subprocess.check_call(cmd, stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
-    except OSError:
-        print('Error: command does not seem to be installed')
-    except subprocess.CalledProcessError:
-        print(error_msg)
+def parse_deployment_properties(fn):
+    deployment_properties = {}
+    with open(fn) as deployment_properties_file:
+        for line in deployment_properties_file.readlines():
+            if line.startswith('#'):
+                # skip comments
+                pass
+            elif '=' in line:
+                k, v = line.split('=')
+                deployment_properties[k] = v.strip()
+    return deployment_properties
+
+
+def get_distribution_url_from_formula(content):
+    url_line = filter(lambda l: l.lstrip().startswith('url'), content.split('\n'))[0]
+    url = url_line.strip().split(' ')[1].replace('"', '')
+    return url
+
+
+def url_with_credential(url, credential):
+    scheme, rest = url.split('://')
+    return scheme + '://"' + credential + '"@' + rest
+
+
+if not os.getenv('GRABL_CREDENTIAL'):
+    print('Error - $GRABL_CREDENTIAL must be defined')
+    sys.exit(1)
+
+
+if len(sys.argv) != 2:
+    print('Error - needs an argument: <test|release>')
+    sys.exit(1)
+
+
+# configurations #
+properties = parse_deployment_properties('deployment.properties')
+formula_filename = os.path.basename(os.readlink('formula'))
+with open('formula') as formula_file:
+    formula_template = formula_file.read()
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+tap_type = sys.argv[1]
+tap_url = properties['repo.brew.{}'.format(tap_type)]
+with open('checksum.sha256') as checksum_file:
+    checksum_of_distribution_local = checksum_file.read().strip().split(' ')[0]
+
+tap_localpath = tempfile.mkdtemp()
+try:
+    print('Cloning brew tap: "{}"...'.format(tap_url))
+    sp.check_call(['git', 'clone', tap_url, tap_localpath])
+    sp.check_call(['mkdir', '-p', 'Formula'], cwd=tap_localpath)
+    formula_content = formula_template.replace('{version}', version).replace('{sha256}', checksum_of_distribution_local)
+    distribution_url = get_distribution_url_from_formula(formula_content)
+    print('Attempting to match the checksums of local distribution and Github distribution from "{}"...'.format(distribution_url))
+    urllib.urlretrieve(distribution_url, 'distribution-github.zip')
+    checksum_of_distribution_github = sp.check_output(['shasum', '-a', '256', 'distribution-github.zip']).split(' ')[0]
+    if checksum_of_distribution_local != checksum_of_distribution_github:
+        print('Error - unable to proceed with deploying to brew! The checksums do not match:')
+        print('- The checksum of local distribution: {}'.format(checksum_of_distribution_local))
+        print('- The checksum of Github distribution: {}'.format(checksum_of_distribution_github))
         sys.exit(1)
+    print('The checksums matched. Proceeding with deploying to brew...')
+    with open(os.path.join(tap_localpath, 'Formula', formula_filename), 'w') as f:
+        f.write(formula_content)
+    sp.check_call(['git', 'add', '.'], cwd=tap_localpath)
+    try:
+        sp.check_call(['git', 'commit', '-m', 'Update the {} formula to {}'.format(formula_filename, version)], cwd=tap_localpath)
+    except sp.CalledProcessError as e:
+        print('Error - unable to proceed with deploying to brew due to the following error:')
+        raise e
 
-
-check_call(['curl', '-f', URL],
-           'Verifying file by URL exists {}'.format(URL),
-           'Error: deployed zip with version {} does not exist'.format(VERSION))
-
-check_call(['brew', 'bump-formula-pr', URL_ARG],
-           'Calling brew bump-formula-pr',
-           'Error: brew failed')
+    sp.check_call(['bash', '-c', 'git push ' + url_with_credential(tap_url, '$GRABL_CREDENTIAL') + ' master'], cwd=tap_localpath)
+    print("Done! Enjoy the beer.")
+finally:
+    shutil.rmtree(tap_localpath)
