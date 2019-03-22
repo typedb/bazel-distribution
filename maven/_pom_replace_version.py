3b17c1ee49dfb10e1bc70cd7e449fe884d102cbc
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-22 14:37:37 +0000

    assemble_maven now takes in workspace_refs. minor code cleanups

---
+++ maven/_pom_replace_version.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+_, preprocessed_template_path, workspace_refs_path, version_file_path, pom_path = sys.argv
+
+with open(preprocessed_template_path, 'r') as template_file, \
+        open(workspace_refs_path, 'r') as refs_file, \
+        open(version_file_path, 'r') as version_file, \
+        open(pom_path, 'w') as pom_file:
+
+    refs = json.loads(refs_file.read().strip())
+    template = template_file.read().strip()
+    version = version_file.read().strip()
+
+    pom = template
+    for workspace in refs['commits']:
+        pom = pom.replace(workspace, refs['commits'][workspace])
+    for workspace in refs['tags']:
+        pom = pom.replace(workspace, refs['tags'][workspace])
+    pom = pom.replace('{pom_version}', version)
+
+    pom_file.write(pom)
