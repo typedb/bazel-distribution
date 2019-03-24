2d6a386b7676efa8d2fc50a72f514f564826b999
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-24 17:00:49 +0000

    code cleanup - import statement follows alphabetical order, simplified repo type checks (which should always be snapshot or release), moved statements that are related close to each other

--- maven/templates/deploy.py
+++ maven/templates/deploy.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-import hashlib
-import sys
-import tempfile
-import subprocess as sp
-from operator import attrgetter
 from xml.etree import ElementTree
 
+import hashlib
+import operator
 import os
-print(os.getcwd())
+import subprocess as sp
+import sys
+import tempfile
 
-MAVEN_REPO_KEY_PREFIX = 'repo.maven.'
+print(os.getcwd())
 
 
 def parse_deployment_properties(fn):
@@ -50,35 +48,27 @@ def upload(url, username, password, local_fn, remote_fn):
             local_fn, upload_status_code))
 
 
-jar_path = "$JAR_PATH"
-pom_file_path = "$POM_PATH"
-
-pom_file_tree = ElementTree.parse(pom_file_path)
-group_id, artifact_id, version = list(map(attrgetter('text'),
-                                          pom_file_tree.getroot()[1:4]))
-
-
-properties = parse_deployment_properties('external/graknlabs_build_tools/deployment.properties')
-valid_keys = [x.replace(MAVEN_REPO_KEY_PREFIX, '') for x in properties if x.startswith(MAVEN_REPO_KEY_PREFIX)]
-
-
 if len(sys.argv) != 5:
     raise ValueError('Should pass <snapshot|release> <maven-username> <maven-password> as arguments')
 
 _, maven_repo_type, version, maven_username, maven_password = sys.argv
 
-if maven_repo_type not in valid_keys:
-    raise ValueError("first argument should be one of {}, not {}".format(valid_keys, maven_repo_type))
+if maven_repo_type not in ['snapshot', 'release']:
+    raise ValueError("first argument should be one of these: {}".format(['snapshot', 'release']))
 
-maven_url = properties[MAVEN_REPO_KEY_PREFIX + maven_repo_type]
-
+deployment_properties = parse_deployment_properties('external/graknlabs_build_tools/deployment.properties')
+maven_url = deployment_properties['repo.maven.' + maven_repo_type]
+jar_path = "$JAR_PATH"
+pom_file_path = "$POM_PATH"
+group_id, artifact_id, version_placeholder = list(map(operator.attrgetter('text'),
+                                                      ElementTree.parse(pom_file_path).getroot()[1:4]))
 filename_base = '{coordinates}/{artifact}/{version}/{artifact}-{version}'.format(
     coordinates=group_id.replace('.', '/'), version=version, artifact=artifact_id)
 
 upload(maven_url, maven_username, maven_password, jar_path, filename_base + '.jar')
 
 with open(pom_file_path, 'r') as pom_original, tempfile.NamedTemporaryFile(delete=True) as pom_updated:
-    updated = pom_original.read().replace('{pom_version}', version)
+    updated = pom_original.read().replace(version_placeholder, version)
     pom_updated.write(updated)
     pom_updated.flush()
     upload(maven_url, maven_username, maven_password, pom_updated.name, filename_base + '.pom')
