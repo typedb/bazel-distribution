cb00bbb65034ec2a09289a8e7a41a8a885fc3059
Max Vorobev <vmax0770@gmail.com>
2019-09-11 12:21:14 +0100

    Verify that referenced workspace exists in apt/rpm (#179)
    ## What is the goal of this PR?
    
    Fix #178
    
    ## What are the changes implemented in this PR?
    
    - `assemble_apt` now takes in `%{version}` - consistent with how it's done in RPM builds
    - `assemble_apt` and `assemble_rpm` now verify that for each `%{@dependent_workspace_ref}`, relevant `@dependent_workspace_ref` exists in `workspace_refs`

--- apt/generate_depends_file.py
+++ apt/generate_depends_file.py
@@ -21,6 +21,9 @@
 
 import argparse
 import json
+import re
+
+WORKSPACE_REF_PATTERN = re.compile(r'.*%{@(?P<workspace_ref>.*)}.*')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output', required=True, help='Output file')
@@ -38,22 +41,33 @@ with open(args.version_file) as f:
     version = f.read().strip()
 
 replacements = {
-    "{version}": version
+    "%{version}": version
 }
 
 if args.workspace_refs:
     with open(args.workspace_refs) as f:
         workspace_refs = json.load(f)
 
+all_workspaces = set()
+
 for ws, commit in workspace_refs['commits'].items():
     replacements["%{{@{}}}".format(ws)] = "0.0.0-" + commit
+    all_workspaces.add(ws)
 
 for ws, tag in workspace_refs['tags'].items():
     replacements["%{{@{}}}".format(ws)] = tag
+    all_workspaces.add(ws)
 
 deps = []
 
 for dep in args.deps:
+    match = WORKSPACE_REF_PATTERN.match(dep)
+    if match:
+        workspace_ref = match.group('workspace_ref')
+        if workspace_ref not in all_workspaces:
+            raise Exception('invalid workspace was referenced: `{}`; valid workspaces to reference are: {}'.format(
+                workspace_ref, list(all_workspaces)
+            ))
     for replacement_key, replacement_val in replacements.items():
         dep = dep.replace(replacement_key, replacement_val)
     deps.append(dep)
