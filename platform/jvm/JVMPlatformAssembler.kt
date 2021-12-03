1a501f07391bf199a9db83abd5ebfc8905c4e889
Alex Walker <alex@vaticle.com>
2021-12-03 12:15:26 +0000

    Platform JVM assembler fixes (#326)
    ## What is the goal of this PR?
    
    The `assemble_jvm_platform` rule was failing to compile due to some broken changes that went in, so we fixed them.
    
    ## What are the changes implemented in this PR?
    
    - Undelete config_setting: apple-code-sign (I thought it was unused but it is actually used in typedb-studio)
    - Correctly unpackage the JDK on Mac/Linux using `tar`, not `jar`

--- platform/jvm/JVMPlatformAssembler.kt
+++ platform/jvm/JVMPlatformAssembler.kt
@@ -33,6 +33,7 @@ import com.vaticle.bazel.distribution.platform.jvm.Logging.Logger
 import com.vaticle.bazel.distribution.platform.jvm.Shell.Programs.JAR
 import com.vaticle.bazel.distribution.platform.jvm.Shell.Programs.JPACKAGE
 import com.vaticle.bazel.distribution.platform.jvm.Shell.Programs.JPACKAGE_EXE
+import com.vaticle.bazel.distribution.platform.jvm.Shell.Programs.TAR
 import java.io.File
 import java.lang.System.getenv
 import java.nio.file.Files
@@ -92,7 +93,10 @@ object JVMPlatformAssembler {
 
         fun extractJDK() {
             createDirectory(Path.of(JDK))
-            shell.execute(command = listOf(JAR, "xf", Path.of("..", options.jdkPath).toString()), baseDir = Path.of(JDK))
+            when (currentOS) {
+                MAC, LINUX -> shell.execute(listOf(TAR, "-xf", options.jdkPath, "-C", JDK))
+                WINDOWS -> shell.execute(command = listOf(JAR, "xf", Path.of("..", options.jdkPath).toString()), baseDir = Path.of(JDK))
+            }
         }
 
         fun extractWiXToolset() {
