9616e6042042b220416bf3c0bd7d05ca1784a2a4
Alex Walker <alex@grakn.ai>
2022-01-07 12:55:04 +0000

    Get all CLI logic into DeployNPM file

--- npm/DeployNPM.kt
+++ npm/DeployNPM.kt
@@ -21,16 +21,78 @@
 
 package com.vaticle.bazel.distribution.npm
 
-import com.vaticle.bazel.distribution.npm.Deployer.Options
+import com.vaticle.bazel.distribution.npm.Options.CommandLineParams.NPM_PATH
+import com.vaticle.bazel.distribution.npm.Options.CommandLineParams.RELEASE_REPO
+import com.vaticle.bazel.distribution.npm.Options.CommandLineParams.SNAPSHOT_REPO
+import com.vaticle.bazel.distribution.npm.Options.Env.DEPLOY_NPM_TOKEN
+import com.vaticle.bazel.distribution.npm.Options.RepositoryType.RELEASE
+import com.vaticle.bazel.distribution.npm.Options.RepositoryType.SNAPSHOT
 import picocli.CommandLine
 
-fun parseCommandLine(args: Array<String>): Deployer.CommandLineParams {
-    val commandLine = CommandLine(Deployer.CommandLineParams())
-    val parseResult: CommandLine.ParseResult = commandLine.parseArgs(*args)
-    assert(parseResult.asCommandLineList().size == 1)
-    return parseResult.asCommandLineList()[0].getCommand()
+class Options {
+    @CommandLine.Option(names = [NPM_PATH], required = true)
+    lateinit var npmPath: String
+
+    @CommandLine.Option(names = [SNAPSHOT_REPO], required = true)
+    private lateinit var snapshotRepo: String
+
+    @CommandLine.Option(names = [RELEASE_REPO], required = true)
+    private lateinit var releaseRepo: String
+
+    @CommandLine.Parameters
+    private lateinit var params: List<String>
+
+    val registryURL: String
+    get() {
+        if (params.isEmpty() || params[0].isBlank()) {
+            throw IllegalArgumentException("Missing required positional argument: <${RepositoryType.allValuesString}>")
+        }
+        return when (RepositoryType.of(params[0])) {
+            SNAPSHOT -> snapshotRepo
+            RELEASE -> releaseRepo
+        }
+    }
+
+    val npmToken: String
+    get() {
+        return System.getenv(DEPLOY_NPM_TOKEN)
+            ?: throw IllegalArgumentException("token should be passed via \$${DEPLOY_NPM_TOKEN} env variable")
+    }
+
+    companion object {
+        fun of(args: Array<String>): Options {
+            val commandLine = CommandLine(Options())
+            val parseResult: CommandLine.ParseResult = commandLine.parseArgs(*args)
+            assert(parseResult.asCommandLineList().size == 1)
+            return parseResult.asCommandLineList()[0].getCommand()
+        }
+    }
+
+    private enum class RepositoryType(val displayName: String) {
+        SNAPSHOT("snapshot"),
+        RELEASE("release");
+
+        companion object {
+            val allValuesString = values().joinToString("|") { it.displayName }
+
+            fun of(displayName: String): RepositoryType {
+                return values().find { it.displayName == displayName }
+                    ?: throw IllegalArgumentException("Invalid repo type: '$displayName' (valid values are <$allValuesString>)")
+            }
+        }
+    }
+
+    object CommandLineParams {
+        const val NPM_PATH = "--npm_path"
+        const val RELEASE_REPO = "--release_repo"
+        const val SNAPSHOT_REPO = "--snapshot_repo"
+    }
+
+    object Env {
+        const val DEPLOY_NPM_TOKEN = "DEPLOY_NPM_TOKEN"
+    }
 }
 
 fun main(args: Array<String>) {
-    Deployer(Options.of(parseCommandLine(args))).deploy()
+    Deployer(Options.of(args)).deploy()
 }
--- npm/Deployer.kt
+++ npm/Deployer.kt
@@ -27,20 +27,11 @@ import com.vaticle.bazel.distribution.common.OS.MAC
 import com.vaticle.bazel.distribution.common.OS.LINUX
 import com.vaticle.bazel.distribution.common.shell.Shell
 import com.vaticle.bazel.distribution.common.util.SystemUtil.currentOS
-import com.vaticle.bazel.distribution.npm.Deployer.CommandLineParams.Keys.NPM_PATH
-import com.vaticle.bazel.distribution.npm.Deployer.CommandLineParams.Keys.RELEASE_REPO
-import com.vaticle.bazel.distribution.npm.Deployer.CommandLineParams.Keys.SNAPSHOT_REPO
-import com.vaticle.bazel.distribution.npm.Deployer.Env.DEPLOY_NPM_TOKEN
-import com.vaticle.bazel.distribution.npm.Deployer.Env.PATH
-import com.vaticle.bazel.distribution.npm.Deployer.Options.RepositoryType.*
-import picocli.CommandLine
 import java.nio.file.Files
 import java.nio.file.Path
 
-class Deployer(options: Options) {
+class Deployer(private val options: Options) {
     private val logger = Logger(logLevel = DEBUG)
-    private val npmPath = options.npmPath
-    private val registryURL = options.registryURL
 
     fun deploy() {
         configureAuthToken()
@@ -48,19 +39,17 @@ class Deployer(options: Options) {
     }
 
     private fun configureAuthToken() {
-        val npmToken = System.getenv(DEPLOY_NPM_TOKEN)
-            ?: throw IllegalArgumentException("token should be passed via \$$DEPLOY_NPM_TOKEN env variable")
-        Files.writeString(Path.of(".npmrc"), "//$registryURL:_authToken=$npmToken")
+        Files.writeString(Path.of(".npmrc"), "//${options.registryURL}:_authToken=${options.npmToken}")
     }
 
     private fun publishPackage() {
         Shell(logger = logger, verbose = true).execute(
-            command = listOf("npm", "publish", "--registry=$registryURL", "deploy_npm.tgz"),
-            env = mapOf(PATH to pathEnv()))
+            command = listOf("npm", "publish", "--registry=${options.registryURL}", "deploy_npm.tgz"),
+            env = mapOf("PATH" to pathEnv()))
     }
 
     private fun pathEnv(): String {
-        val commonPaths = listOf(realPath(npmPath).parent)
+        val commonPaths = listOf(realPath(options.npmPath).parent)
         val otherPaths = when (currentOS) {
             MAC, LINUX -> listOf("/usr/bin", "/bin/")
             else -> listOf()
@@ -74,59 +63,5 @@ class Deployer(options: Options) {
             true -> pathObj.toRealPath()
             false -> pathObj
         }
-    }
-
-    data class Options(val npmPath: String, val registryURL: String) {
-        companion object {
-            fun of(commandLineParams: CommandLineParams): Options {
-                if (commandLineParams.params.isEmpty() || commandLineParams.params[0].isBlank()) {
-                    throw IllegalArgumentException("Missing required positional argument: <${RepositoryType.allValuesString}>")
-                }
-                val registryURL = when (RepositoryType.of(commandLineParams.params[0])) {
-                    SNAPSHOT -> commandLineParams.snapshotRepo
-                    RELEASE -> commandLineParams.releaseRepo
-                }
-                return Options(commandLineParams.npmPath, registryURL)
-            }
-        }
-
-        enum class RepositoryType(val displayName: String) {
-            SNAPSHOT("snapshot"),
-            RELEASE("release");
-
-            companion object {
-                val allValuesString = values().joinToString("|") { it.displayName }
-
-                fun of(displayName: String): RepositoryType {
-                    return values().find { it.displayName == displayName }
-                        ?: throw IllegalArgumentException("Invalid repo type: '$displayName' (valid values are <$allValuesString>)")
-                }
-            }
-        }
-    }
-
-    class CommandLineParams {
-        @CommandLine.Option(names = [NPM_PATH], required = true)
-        lateinit var npmPath: String
-
-        @CommandLine.Option(names = [SNAPSHOT_REPO], required = true)
-        lateinit var snapshotRepo: String
-
-        @CommandLine.Option(names = [RELEASE_REPO], required = true)
-        lateinit var releaseRepo: String
-
-        @CommandLine.Parameters
-        lateinit var params: List<String>
-
-        object Keys {
-            const val NPM_PATH = "--npm_path"
-            const val RELEASE_REPO = "--release_repo"
-            const val SNAPSHOT_REPO = "--snapshot_repo"
-        }
-    }
-
-    object Env {
-        const val DEPLOY_NPM_TOKEN = "DEPLOY_NPM_TOKEN"
-        const val PATH = "PATH"
     }
 }
