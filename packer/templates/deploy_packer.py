03781359260522a3cf8920278a80b3e1ca3b024d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 22:16:19 +0000

    Adding tarfile member sanitization to extractall()

--- packer/templates/deploy_packer.py
+++ packer/templates/deploy_packer.py
@@ -40,7 +40,26 @@ TARGET_TAR_LOCATION = "{target_tar}"
 
 target_temp_dir = tempfile.mkdtemp('deploy_packer')
 with tarfile.open(TARGET_TAR_LOCATION, 'r') as target_tar:
-    target_tar.extractall(target_temp_dir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(target_tar, target_temp_dir)
 
 args = [
     PACKER_BINARIES[system],
