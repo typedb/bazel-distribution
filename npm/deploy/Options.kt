a59a4e4209acc8e3053d5ba1a13a7a0241ae420f
Joshua Send <joshua@vaticle.com>
2023-03-09 10:09:41 +0000

    Extend NPM deployment with password or token-based deployment (#372)
    ## What is the goal of this PR?
    
    As of January 2023, the `deploy_npm` using npm v8.x rule appears to stop working with pre-generated auth tokens for Sonatype repositories. However, we discover that the simpler mechanism of using base64 encoded username/password does work.
    
    Using the base64 encoded authenciation is also [recommended](https://help.sonatype.com/repomanager3/nexus-repository-administration/formats/npm-registry/npm-security#npmSecurity-AuthenticationUsingBasicAuth) by Sonatype, we can successfully deploy.
    
    However, the deployment to centralised NPM repositories may still require using token-based authentication. As a result, we extend the deployment rule to support both username/password or token-based authentication.
    
    This issue was last worked on in https://github.com/vaticle/bazel-distribution/pull/330
    
    ## What are the changes implemented in this PR?
    
    * Introduce the capability to use username+password authentication OR token authentication based on which environment variables are present

--- npm/deploy/Options.kt
+++ npm/deploy/Options.kt
@@ -1,9 +1,29 @@
+/*
+ * Copyright (C) 2022 Vaticle
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.vaticle.bazel.distribution.npm.deploy
 
 import com.vaticle.bazel.distribution.npm.deploy.Options.CommandLineParams.NPM_PATH
 import com.vaticle.bazel.distribution.npm.deploy.Options.CommandLineParams.RELEASE_REPO
 import com.vaticle.bazel.distribution.npm.deploy.Options.CommandLineParams.SNAPSHOT_REPO
-import com.vaticle.bazel.distribution.npm.deploy.Options.Env.DEPLOY_NPM_TOKEN
 import com.vaticle.bazel.distribution.npm.deploy.Options.RepositoryType.RELEASE
 import com.vaticle.bazel.distribution.npm.deploy.Options.RepositoryType.SNAPSHOT
 import picocli.CommandLine
@@ -32,10 +52,19 @@ class Options {
             }
         }
 
-    val npmToken: String
+    val npmToken: String?
         get() {
-            return System.getenv(DEPLOY_NPM_TOKEN)
-                ?: throw IllegalArgumentException("token should be passed via \$${DEPLOY_NPM_TOKEN} env variable")
+            return System.getenv().get(Env.DEPLOY_NPM_TOKEN);
+        }
+
+    val npmUsername: String?
+        get() {
+            return System.getenv().get(Env.DEPLOY_NPM_USERNAME)
+        }
+
+    val npmPassword: String?
+        get() {
+            return System.getenv().get(Env.DEPLOY_NPM_PASSWORD)
         }
 
     companion object {
@@ -68,5 +97,7 @@ class Options {
 
     object Env {
         const val DEPLOY_NPM_TOKEN = "DEPLOY_NPM_TOKEN"
+        const val DEPLOY_NPM_USERNAME = "DEPLOY_NPM_USERNAME"
+        const val DEPLOY_NPM_PASSWORD = "DEPLOY_NPM_PASSWORD"
     }
 }
