24111e53404b866b8b21a765c63b074b3eb24a09
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-09-08 12:02:45 +0100

    remove dev/stderr (win issue fix?)

--- maven/templates/deploy.py
+++ maven/templates/deploy.py
@@ -41,7 +41,7 @@ def md5(fn):
 
 def upload(url, username, password, local_fn, remote_fn):
     upload_status_code = sp.check_output([
-        'curl', '--silent', '--output', '/dev/stderr',
+        'curl', '--silent',
         '--write-out', '%{http_code}',
         '-u', '{}:{}'.format(username, password),
         '--upload-file', local_fn,
